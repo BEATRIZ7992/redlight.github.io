1f3d886699a5aa150738ccb29622901985584557
BEATRIZ7992 <77105759+BEATRIZ7992@users.noreply.github.com>
2024-06-07 19:03:59 +0200

    Add files via upload

---
+++ public/service.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('rlg-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/game.html',
+        '/styles.css',
+        '/app.js',
+        '/manifest.json',
+        'images/red-light.png',
+        'images/green-light.png',
+        'images/shoe1.png',
+        'images/shoe2.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
